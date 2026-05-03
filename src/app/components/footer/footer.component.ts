52c2384af772fa794626fbc0ddf9d05859d81399
Shashwat Singh Bhadoriya <shashsinghbhadoriya@gmail.com>
2026-05-03 23:16:31 +0530

    feat: implement AJAX form submission for Formspree

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  submitting = false;
+  submitted = false;
+  error = false;
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    this.submitting = true;
+    this.error = false;
+
+    this.http.post('https://formspree.io/f/mojrjoeg', formData).subscribe({
+      next: () => {
+        this.submitting = false;
+        this.submitted = true;
+        form.reset();
+      },
+      error: () => {
+        this.submitting = false;
+        this.error = true;
+      }
+    });
+  }
 }
